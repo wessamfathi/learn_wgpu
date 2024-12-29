57004344f0471ea710121de3043b5cc4f89cc89a
Wessam Ibrahim <wessamfathi@gmail.com>
2024-12-29 20:57:36 +0100

    Create window

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+use std::process::Termination;
+
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
+
+pub fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let res = event_loop.run(move |event, control_flow| match event {
+        Event::WindowEvent { 
+            window_id, 
+            ref event 
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested 
+            | WindowEvent::KeyboardInput { 
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => control_flow.exit(),
+            _ => {}
+        },
+        _ => {}
+    });
+
+    println!("result is: {:?}", res.unwrap().report());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    learn_wgpu::run();
 }
